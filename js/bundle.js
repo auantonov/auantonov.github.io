2e5126f742a47a7b10c79753b90595c7e98ec546
noxonsu <myzonemailru@gmail.com>
2016-12-09 03:33:46 +1000

    da

--- js/bundle.js
+++ js/bundle.js
@@ -1412,7 +1412,7 @@ Main.createAccount = function() {
   var addr = newAccount.address;
   var pk = newAccount.privateKey;
   Main.addAccount(addr, pk);
-  Main.alertDialog('You just created an Ethereum account: '+addr+'<br /><br />Please BACKUP the private key for this account: '+pk);
+  //Main.alertDialog('You just created an Ethereum account: '+addr+'<br /><br />Please BACKUP the private key for this account: '+pk);
   return newAccount;
  }
 Main.deleteAccount = function() {

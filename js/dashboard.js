36e61c0592b3aaaad6074252322ffeee22366cb5
DmitriOkulov <dmitriokulov@yandex.ru>
2017-03-22 01:59:48 +0500

    Округлил чуток

--- js/dashboard.js
+++ js/dashboard.js
@@ -267,7 +267,7 @@ function bundle_loaded() {
 
             //add_token(item.inteval,item.tip,item.start,item.price,item.lastprice,item.startprice,item.ticker,item.rel,item.title);
             var line;
-            line += "<tr class='pie firstline' data-token='" + item.token + "' data-tid='" + item.id + "' data-tokensold='" + item.mint + "' data-interval='" + item.interv + "' data-tip='" + item.tip + "' data-start='" + item.start + "' data-price='" + item.price + "' data-lastprice='" + item.price + "' data-startprice='" + item.lastprice + "' data-ticker='" + item.ticker + "' rel='" + item.rel + "' data-masterkey='"+ item.masterkey +"' data-bitcoinadr='"+item.bitcoinadr+"' data-count-token='"+item.count_token+"'  data-prirost='"+item.prirost+"'>";
+            line += "<tr class='pie firstline' data-token='" + item.token + "' data-tid='" + item.id + "' data-tokensold='" + item.mint + "' data-interval='" + item.interv + "' data-tip='" + item.tip + "' data-start='" + item.start + "' data-price='" + item.price + "' data-lastprice='" + item.price + "' data-startprice='" + item.lastprice + "' data-ticker='" + item.ticker + "' rel='" + item.rel + "' data-masterkey='"+ item.masterkey +"' data-bitcoinadr='"+item.bitcoinadr+"' data-count-token='"+item.count_token+"'  data-prirost='"+parseFloat(item.prirost).toFixed(2)+"'>";
             line += "<td class='pai-title'>" + item.title + "</td>";
             line += "<td class='tip'>" + item.tip + "</td>";
             line += "<td class='number'><span class='currentprice'>"+parseFloat(item.now_price).toFixed(2)+"</span> р</td>";
